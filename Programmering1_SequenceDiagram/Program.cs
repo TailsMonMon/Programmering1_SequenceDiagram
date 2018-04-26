41e42f9993c4da0a679a78feb24994dd36a531c0
Monica Björk <m.bjork95@gmail.com>
2018-04-26 11:47:11 +0200

    Exeption handeling. Comments.

--- Programmering1_SequenceDiagram/Program.cs
+++ Programmering1_SequenceDiagram/Program.cs
@@ -7,23 +7,36 @@ using System.Threading.Tasks;
 namespace Programmering1_SequenceDiagram {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Enter age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            int money = 15000;
+            var age = 0; // Defining data.
+
+            //  INPUT
+            do {
+                Console.Write("Enter age: ");
+                try {
+                    age = Convert.ToInt32(Console.ReadLine()); // Converts the input string to an int.
+                }
+                catch(FormatException) {
+                    Console.WriteLine("Please write an integer."); // Error message
+                    Console.WriteLine();
+                }
+            } while(age == 0); // When age is chaged from 0. The program will continue.
+
+            int money = 15000; // Defining data.
 
             do {
                 if(age < 60 || money > 20000) {
                     Console.WriteLine("You are saving a little."); //FIXME sic.
-                    money += 500;
+                    money += 500; // Adds 500 to money.
                 }
                 else {
                     Console.WriteLine("You are saving a lot");
-                    money += 1000;
+                    money += 1000; // Adds 1000 to money.
                 }
 
-                age++;
-            } while(money < 20000 && age < 65);
+                age++; // Adds 1 to age.
+            } while(money < 20000 && age < 65); // While money is less than 20 000 AND age is less than 65
 
+            //  OUTPUT
             Console.WriteLine("You are retired by the age of {0}.", age);
             Console.WriteLine("You are saved {0} dollars.", money); // FIXME sic.
             Console.ReadKey();
